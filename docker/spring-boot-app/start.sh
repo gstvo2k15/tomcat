5a0246f62feb4ca3bcddc85a18bc85d8148ca177
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-21 13:06:36 +0100

    Include ArgoCD sync to pipeline-1d

--- docker/spring-boot-app/start.sh
+++ docker/spring-boot-app/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Download WAR from Nexus..."
-curl -u "${NEXUS_USER}:${NEXUS_PASS}" -o /usr/local/tomcat/webapps/uvc.war \
-  "${NEXUS_URL}/uvc-${BUILD_NUMBER}.war"
+export NEXUS_USER=$(cat /secrets/username)
+export NEXUS_PASS=$(cat /secrets/password)
 
-echo "Starting Tomcat..."
-catalina.sh run
+echo "Downloading WAR file from Nexus..."
+curl -u "$NEXUS_USER:$NEXUS_PASS" \
+     -o /usr/local/tomcat/webapps/uvc.war \
+     "$NEXUS_URL/uvc-${BUILD_NUMBER}.war"
+
+exec catalina.sh run
